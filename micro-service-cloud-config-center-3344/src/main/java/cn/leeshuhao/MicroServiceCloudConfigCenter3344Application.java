4b8a18f0313829a3b9f1ecf50a6d95e269bd0ef7
MrLee <li.shuhao@lifecycle.cn>
2022-10-08 18:38:11 +0800

    add

---
+++ micro-service-cloud-config-center-3344/src/main/java/cn/leeshuhao/MicroServiceCloudConfigCenter3344Application.java
@@ -0,0 +1,20 @@
+package cn.leeshuhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <p>Micro Service</p>
+ *
+ * @author MrLee
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class MicroServiceCloudConfigCenter3344Application {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceCloudConfigCenter3344Application.class, args);
+    }
+}
---
+++ micro-service-cloud-gateway-9527/src/main/java/cn/leeshuhao/MicroServiceCloudGateway9527Application.java
@@ -0,0 +1,18 @@
+package cn.leeshuhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <p></p>
+ *
+ * @author MrLee
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroServiceCloudGateway9527Application {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceCloudGateway9527Application.class, args);
+    }
+}
